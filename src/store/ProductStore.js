577d06cd752ed7df1edb33305b8c44962b1337b3
soorajhoysalka <surajhoysal817@gmail.com>
2023-12-06 22:46:16 +0530

    ip changes to surajs laptop

--- src/store/ProductStore.js
+++ src/store/ProductStore.js
@@ -9,34 +9,34 @@ export const useProductRootStore = defineStore("productRoot", () => {
   const productsOfFashion = ref([]);
 
   const FETCH_PRODUCTS = async () => {
-    const res = await fetch("http://10.20.3.72:8099/api/products/all");
+    const res = await fetch("http://10.20.3.164:8099/api/products/all");
     const jsonnew = await res.json();
     products.value = jsonnew;
   };
   const FETCH_ELECRONICS_PRODUCTS = async () => {
     const res = await fetch(
-      "http://10.20.3.72:8099/api/products/by-category/Electronics"
+      "http://10.20.3.164:8099/api/products/by-category/Electronics"
     );
     const jsonnew = await res.json();
     productsOfElectronics.value = jsonnew;
   };
   const FETCH_BOOKS_PRODUCTS = async () => {
     const res = await fetch(
-      "http://10.20.3.72:8099/api/products/by-category/Books"
+      "http:/10.20.3.164:8099/api/products/by-category/Books"
     );
     const jsonnew = await res.json();
     productsOfBooks.value = jsonnew;
   };
   const FETCH_FASHION_PRODUCTS = async () => {
     const res = await fetch(
-      "http://10.20.3.72:8099/api/products/by-category/Fashion"
+      "http://10.20.3.164:8099/api/products/by-category/Fashion"
     );
     const jsonnew = await res.json();
     productsOfFashion.value = jsonnew;
   };
 
   const FETCH_PRODUCT_BY_ID = async (productId) => {
-    const res = await fetch(`http://10.20.3.72:8099/api/products/${productId}`);
+    const res = await fetch(`http://10.20.3.164:8099/api/products/${productId}`);
     const jsonnew = await res.json();
     LastesProductById.value = { ...jsonnew };
   };
--- src/store/auth-store.js
+++ src/store/auth-store.js
@@ -18,7 +18,7 @@ export const useAuthStore = defineStore("auth", () => {
         body: JSON.stringify(userCredentials),
       };
 
-      const res = await fetch("http://10.20.3.72:8051/api/auth/login", options);
+      const res = await fetch("http://10.20.3.164:8051/api/auth/login", options);
 
       if (!res.ok) {
         console.error("Error during login:", res.status);
@@ -46,7 +46,7 @@ export const useAuthStore = defineStore("auth", () => {
       };
 
       const res = await fetch(
-        "http://10.20.3.72:8051/api/users/register",
+        "http://10.20.3.164:8051/api/users/register",
         options
       );
       const jsonResult = await res.json();
